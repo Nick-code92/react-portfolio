b8b00d7222c14934a3efa6da962e3d781e3adea4
Nikddimos Gebregiorgis <gebrenick07@gmail.com>
2020-02-29 18:28:15 -0700

    creating a navbar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,56 @@
 import React from 'react';
+import Container from "react-bootstrap/Container";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import Navbar from "react-bootstrap/Navbar";
+import Nav from "react-bootstrap/Navbar";
 
-function App() {
-  return (
-    <div className="App">
-      Hi
-    </div>
-  );
-}
+class App extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      title: `Nikddimos Gebregiorgis`,
+      headerLink: [
+        { title: "Portfolio", path: "/" },
+        { title: "About", path: "/about" },
+        { title: "Contact", path: "/contact" }
+      ],
+      portfolio: {
+        title: "I'm a junior developer",
+        subTitle: "projects that made a difference",
+        text: "Checkout my projects below"
+      },
+      about: {
+        title: "About Me",
+      },
+      contact: {
+        title: "You can contact me anytime", 
+      }
+    }
+  }
+
+  render (){
+    return (
+     <Router>
+       <Container className="p-o" fluid={true}>
+         <Navbar className="border-bottom">
+           <Navbar.Brand>Nikddimos Gebregiorgis</Navbar.Brand>
+           <Navbar.Toggle aria-controls="navbar-toggle" />
+           <Navbar.Collapse id="navbar-toggle">
+            <Nav className="ml-auto">
+              <Link className="nav-link" to="/">Portfolio</Link>
+              <Link className="nav-link" to="/about">About</Link>
+              <Link className="nav-link" to="/contact">Contact</Link>
+            </Nav>
+           </Navbar.Collapse>
+         </Navbar>
+
+         
+       </Container>
+
+     </Router>
+    );
+  }
+  }
 
 export default App;
